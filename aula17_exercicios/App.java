d38474380acd2dfe8ce5fda934548e5bcd1d22ae
leonardoheredia <leonardo@heredia.com.br>
2024-04-22 22:13:24 -0300

    Aula 17 - Exercicios

--- aula17_exercicios/App.java
+++ aula17_exercicios/App.java
@@ -1,18 +1,12 @@
 package aula17_exercicios;
 
-import aula15_filas_dinamica.FilaDinamica;
 
 public class App {
     public static void main(String[] args) {
-        aula15_filas_dinamica.FilaDinamica f = new FilaDinamica();
-        f.enfileirar("Ana");
-        f.enfileirar("Felipe");
-        f.enfileirar("Guilherme");
-        System.out.println(f);
-        System.out.println("Tirar a --> " + f.desenfileirar());
-        System.out.println(f);
-        System.out.println(f.desenfileirar());
-        System.out.println(f.desenfileirar());
-        System.out.println(f.desenfileirar());
+        PilhaDinamica pilha = new PilhaDinamica();
+        pilha.empilhar("Joao");
+        pilha.empilhar("Maria");
+        pilha.empilhar("Jose");
+        System.out.println(pilha);
     }
 }
--- aula17_exercicios/PilhaDinamica.java
+++ aula17_exercicios/PilhaDinamica.java
@@ -1,5 +1,62 @@
 package aula17_exercicios;
 
 public class PilhaDinamica {
+    class Nodo {
+        String item;
+        Nodo anterior;
+        public Nodo(String item) {
+            this.item = item;
+            this.anterior = null;
+        }
+    }
+    private Nodo base;
+    private Nodo topo;
+    private int tamanho;
+    public PilhaDinamica() {
+        tamanho = 0;
+        base = null;
+        topo = null;
+    }
+    public void empilhar(String item) {
+        Nodo n = new Nodo(item);
+        if(tamanho==0) {
+            base = n;
+            topo = n;
+        }
+        else {
+            n.anterior = topo;
+            topo = n;
+        }
+        tamanho++;
+    }
+    public String desempilhar() {
+        if(tamanho==0) return null;
 
+        String valor = topo.item;
+        if(topo.anterior!=null) {
+            topo = topo.anterior;
+        }
+        tamanho--;
+        return valor;
+    }
+    public void limpar() {
+        base = null;
+        topo = null;
+        tamanho = 0;
+    };
+    public void mergear(PilhaDinamica outraPilha) {
+
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator());
+        Nodo aux = topo;
+        while(aux!=null) {
+            sb.append(aux.item).append(System.lineSeparator());
+            aux = aux.anterior;
+        }
+        sb.append("base = " + base.item + ", topo = " + topo.item);
+        return sb.toString();
+    }
 }
